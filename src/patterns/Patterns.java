eca9612a3e5e109a69be106791110b75954c1d05
Jxoul <jxoul.k@gmail.com>
2025-11-12 22:09:28 +0530

    LInked list

---
+++ src/patterns/Patterns.java
@@ -0,0 +1,24 @@
+package patterns;
+
+
+public class Patterns {
+    public static void pattern1(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern2(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+}
